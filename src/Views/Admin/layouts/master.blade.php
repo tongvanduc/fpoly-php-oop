3083104efee4340a8cc30c101b97cdd6b99b0595
Tống Văn Đức <tongduc315@gmail.com>
2024-06-04 13:10:01 +0700

    Ghép theme

--- src/Views/Admin/layouts/master.blade.php
+++ src/Views/Admin/layouts/master.blade.php
@@ -1,34 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zxx">
+
+<!-- Mirrored from demo.dashboardpack.com/sales-html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 24 May 2024 07:24:13 GMT -->
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <title>@yield('title')</title>
 
-    <!-- Latest compiled and minified CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Latest compiled JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    @include('layouts.partials.css')
 </head>
 
-<body>
-    <div class="container">
-        <nav class="mt-5">
-            <a href="{{ url('admin') }}">Dashboard</a>
-            <a href="{{ url('admin/products') }}">Quản lý Sản phẩm</a>
-        </nav>
+<body class="crm_body_bg">
 
-        <h1 class="mt-5 mb-3 text-center">@yield('title')</h1>
-        
-        <div class="row">
-            @yield('content')
+
+    @include('layouts.partials.nav')
+
+    <section class="main_content dashboard_part large_header_bg">
+
+        @include('layouts.partials.topbar')
+
+        <div class="main_content_iner overly_inner ">
+            <div class="container-fluid p-0 ">
+
+                @yield('content')
+
+            </div>
         </div>
+
+        @include('layouts.partials.footer')
+    </section>
+
+
+    <div id="back-top" style="display: none;">
+        <a title="Go to Top" href="#">
+            <i class="ti-angle-up"></i>
+        </a>
     </div>
 
+    @include('layouts.partials.js')
 </body>
+
+<!-- Mirrored from demo.dashboardpack.com/sales-html/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 24 May 2024 07:24:13 GMT -->
 
 </html>
--- src/Views/Admin/products/index.blade.php
+++ src/Views/Admin/products/index.blade.php
@@ -5,59 +5,81 @@
 @endsection
 
 @section('content')
-    @if (isset($_SESSION['status']) && $_SESSION['status'])
-        <div class="alert alert-success">{{ $_SESSION['msg'] }}</div>
+    <div class="row justify-content-center">
+        <div class="col-lg-12">
+            <div class="white_card card_height_100 mb_30">
+                <div class="white_card_header">
+                    <div class="box_header m-0">
+                        <div class="main-title">
+                            <h1 class="m-0">Danh sách</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="white_card_body">
 
-        @php
-            unset($_SESSION['status']);
-            unset($_SESSION['msg']);
-        @endphp
-    @endif
+                    <div class="table-responsive">
+                        @if (isset($_SESSION['status']) && $_SESSION['status'])
+                            <div class="alert alert-success">{{ $_SESSION['msg'] }}</div>
 
-    @if (isset($_SESSION['status']) && !$_SESSION['status'])
-        <div class="alert alert-warning">{{ $_SESSION['msg'] }}</div>
+                            @php
+                                unset($_SESSION['status']);
+                                unset($_SESSION['msg']);
+                            @endphp
+                        @endif
 
-        @php
-            unset($_SESSION['status']);
-            unset($_SESSION['msg']);
-        @endphp
-    @endif
+                        @if (isset($_SESSION['status']) && !$_SESSION['status'])
+                            <div class="alert alert-warning">{{ $_SESSION['msg'] }}</div>
 
-    <a href="{{ url("admin/products/create") }}" class="btn btn-primary">Thêm mới</a>
+                            @php
+                                unset($_SESSION['status']);
+                                unset($_SESSION['msg']);
+                            @endphp
+                        @endif
 
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>IMG THUMBNAIL</th>
-                <th>NAME</th>
-                <th>Category Name</th>
-                <th>CREATED AT</th>
-                <th>UPDATED AT</th>
-                <th>ACTION</th>
-            </tr>
-        </thead>
-        <tbody>
+                        <a href="{{ url('admin/products/create') }}" class="btn btn-primary">Thêm mới</a>
 
-            @foreach ($products as $product)
-                <tr>
-                    <td>{{ $product['id'] }}</td>
-                    <td>
-                        <img src="{{ asset($product['img_thumbnail']) }}" width="100px" alt="">
-                    </td>
-                    <td>{{ $product['name'] }}</td>
-                    <td>{{ $product['c_name'] }}</td>
-                    <td>{{ $product['created_at'] }}</td>
-                    <td>{{ $product['updated_at'] }}</td>
-                    <td>
-                        <a href="{{ url("admin/products/{$product['id']}/show") }}" class="btn btn-info">Xem</a>
-                        <a href="{{ url("admin/products/{$product['id']}/edit") }}" class="btn btn-warning">Sửa</a>
-                        <a href="{{ url("admin/products/{$product['id']}/delete") }}"
-                            onclick="return confirm('Chắc chắn xóa không?');" class="btn btn-danger">Xóa</a>
-                    </td>
-                </tr>
-            @endforeach
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>IMG THUMBNAIL</th>
+                                    <th>NAME</th>
+                                    <th>Category Name</th>
+                                    <th>CREATED AT</th>
+                                    <th>UPDATED AT</th>
+                                    <th>ACTION</th>
+                                </tr>
+                            </thead>
+                            <tbody>
 
-        </tbody>
-    </table>
+                                @foreach ($products as $product)
+                                    <tr>
+                                        <td>{{ $product['id'] }}</td>
+                                        <td>
+                                            <img src="{{ asset($product['img_thumbnail']) }}" width="100px" alt="">
+                                        </td>
+                                        <td>{{ $product['name'] }}</td>
+                                        <td>{{ $product['c_name'] }}</td>
+                                        <td>{{ $product['created_at'] }}</td>
+                                        <td>{{ $product['updated_at'] }}</td>
+                                        <td>
+                                            <a href="{{ url("admin/products/{$product['id']}/show") }}"
+                                                class="btn btn-info">Xem</a>
+                                            <a href="{{ url("admin/products/{$product['id']}/edit") }}"
+                                                class="btn btn-warning">Sửa</a>
+                                            <a href="{{ url("admin/products/{$product['id']}/delete") }}"
+                                                onclick="return confirm('Chắc chắn xóa không?');"
+                                                class="btn btn-danger">Xóa</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
